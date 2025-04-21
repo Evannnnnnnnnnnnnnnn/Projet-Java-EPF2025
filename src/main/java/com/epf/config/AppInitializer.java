942c30366f5cb3bd2bdde934738d4bdd756a0ce8
Evan <evan.pasteau@gmail.com>
2025-04-21 18:35:02 +0200

    Décommenté la magie noir + maven clean install

--- src/main/java/com/epf/config/AppInitializer.java
+++ src/main/java/com/epf/config/AppInitializer.java
@@ -1,23 +1,23 @@
-// package com.epf.config;
+package com.epf.config;
 
-// import org.springframework.lang.NonNull;
-// import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-// public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-//     @Override
-//     protected Class<?>[] getRootConfigClasses() {
-//         return new Class[] {WebConfig.class};
-//     }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
 
-//     @Override
-//     protected Class<?>[] getServletConfigClasses() {
-//         return null;
-//     }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
 
-//     @Override
-//     @NonNull
-//     protected String[] getServletMappings() {
-//         return new String[] { "/" };
-//     }
-// }
+    @Override
+    @NonNull
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
